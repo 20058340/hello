bb0519fb2d10e17bc0f567c1c33b6c39259d2902
20058340 <20058340@mydbs.ie>
2024-09-29 23:13:02 +0530

    changes

--- euler1.js
+++ euler1.js
@@ -1,7 +1,6 @@
 alert("Euler task 2")
 function euler(a,b,n){
-    let a=sum
-    let a = 0;
+    let sum = 0;
     for (let i = 0; i < n; i++){
         if (i % a === 0 || i % b === 0){
             sum = sum + i;
