c5f35f95f7f31862466ab5543d7759130c20129b
20058340 <20058340@mydbs.ie>
2024-09-29 23:07:04 +0530

    added another js file

---
+++ euler1.js
@@ -0,0 +1,14 @@
+alert("Euler task 2")
+function euler(a,b,n){
+    let a=sum
+    let a = 0;
+    for (let i = 0; i < n; i++){
+        if (i % a === 0 || i % b === 0){
+            sum = sum + i;
+        }
+    }
+    return sum
+}
+
+let answer = euler(3,5,1000);
+alert("Sum" +answer)
