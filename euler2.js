8d8cf5a393e23e8290e2e70330b330290b5f13e4
20058340 <20058340@mydbs.ie>
2024-10-09 03:35:34 +0530

    changes

--- euler2.js
+++ euler2.js
@@ -1,20 +1,19 @@
-alert("Euler task 3")
 function summul(a,b,n){
     let sum = 0;
     for (let i = 0; i < n; i++){
         if (i % a === 0 || i % b === 0){
-            sum = sum+i;
+            sum = sum + i;
         }
     }
     return sum
 }
 
-function summlip(){
+function summulip(){
     let a = parseInt(document.getElementById("a").value);
     let b = parseInt(document.getElementById("b").value);
     let x = parseInt(document.getElementById("x").value);
-    let result = summul(a,b,x);
-    alert("sum :" +result)
+    let answer = summul(a,b,x);
+    alert("sum = " + answer)
 }
 
 
