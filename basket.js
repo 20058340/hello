c4cf2c34f2d63a648c1fdf0612be49f2b6706365
20058340 <20058340@mydbs.ie>
2024-10-18 18:03:06 +0530

    changes

--- basket.js
+++ basket.js
@@ -1,5 +1,5 @@
 alert("Basket weight")
-let f1 = (basket,prices)=>{
+let A = (basket,prices)=>{
     let totalCost = 0;
     for(let item in basket){
         if(prices[item] != undefined){
@@ -10,4 +10,4 @@ let f1 = (basket,prices)=>{
 }
 basket = {apple:1,orange:1,grapes:5};
 price = {apple:2.50,orange:3.50,grapes:5.0,tea:10,milk:50};
-alert(f1(basket,price));
+alert(A(basket,price));
